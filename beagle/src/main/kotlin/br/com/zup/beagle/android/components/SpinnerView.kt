652fd9feab055065e76c0b1a40893e05be09ffdd
Shikhar Shrivastav <shikhar2005@gmail.com>
2022-02-18 16:41:24 +0530

    onSelected context update

--- beagle/src/main/kotlin/br/com/zup/beagle/android/components/SpinnerView.kt
+++ beagle/src/main/kotlin/br/com/zup/beagle/android/components/SpinnerView.kt
@@ -2,10 +2,14 @@ package br.com.zup.beagle.android.components
 
 import android.content.Context
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.FrameLayout
 import android.widget.Spinner
 import br.com.zup.beagle.R
+import br.com.zup.beagle.android.action.Action
+import br.com.zup.beagle.android.context.ContextData
+import br.com.zup.beagle.android.utils.handleEvent
 import br.com.zup.beagle.android.widget.RootView
 import br.com.zup.beagle.android.widget.WidgetView
 import br.com.zup.beagle.annotation.RegisterWidget
@@ -13,16 +17,35 @@ import br.com.zup.beagle.annotation.RegisterWidget
 @RegisterWidget("beagleSpinner")
 class SpinnerView (
     private val entries: List<String>,
+    val onSelected: List<Action>? = null,
 ) : WidgetView() {
 
     override fun buildView(rootView: RootView): View {
         return SpinnerComponent(rootView.getContext()).apply {
             setItems(entries)
+            beagleSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    onSelected?.let {
+                        this@SpinnerView.handleEvent(
+                            rootView,
+                            beagleSpinner,
+                            it,
+                            ContextData(
+                                id = "onSelected",
+                                value = entries[position]
+                            )
+                        )
+                    }
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
+            }
         }
     }
 
     class SpinnerComponent(context: Context) : FrameLayout(context) {
-        private val beagleSpinner: Spinner
+        val beagleSpinner: Spinner
 
         init {
             View.inflate(context, R.layout.beagle_spinner_view, this)
